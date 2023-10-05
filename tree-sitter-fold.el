c8667b2fae736c9c40d63e766105d47d52043745
Junyi Hou <junyi.hou@uber.com>
2023-10-05 14:16:11 -0700

    replace tree-sitter with the built-in treesit

--- tree-sitter-fold.el
+++ tree-sitter-fold.el
@@ -1,10 +1,10 @@
 ;;; tree-sitter-fold.el --- code folding using tree-sitter -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2021  Junyi Hou
+;; Copyright (C) 2023  Junyi Hou
 ;;
 ;; Author: Junyi Hou <junyi.yi.hou@gmail.com>
-;; Version: 0.0.1
-;; Package-Requires: ((emacs "25.1") (tree-sitter "0.15.1") (dash "2.19.0"))
+;; Version: 0.2
+;; Package-Requires: ((emacs "29.1") (dash "2.19.0"))
 ;; SPDX-License-Identifier: MIT
 
 ;;; Commentary:
@@ -16,8 +16,7 @@
 ;; `tree-sitter-fold-mode' can properly fold codes.
 
 ;;; Code:
-
-(require 'tree-sitter)
+(require 'treesit)
 (require 'dash)
 
 ;; =============
@@ -30,23 +29,23 @@
   :prefix "tree-sitter-fold-")
 
 (defcustom tree-sitter-fold-foldable-node-alist
-  '((python-mode . (function_definition class_definition)) ;
-    (go-mode . (type_declaration function_declaration method_declaration))
-    (ess-r-mode . (brace_list))
-    (nix-mode . (attrset function)))
+  '((python-ts-mode . ("function_definition" "class_definition")) ;
+    (go-mode . ("type_declaration" "function_declaration" "method_declaration"))
+    (ess-r-mode . ("brace_list"))
+    (nix-mode . ("attrset" "function")))
   "An alist of (mode . (list of tree-sitter-nodes considered foldable in this mode))."
-  :type '(alist :key-type symbol :value-type (repeat symbol))
+  :type '(alist :key-type symbol :value-type (repeat string))
   :group 'tree-sitter-fold)
 
 (defcustom tree-sitter-fold-range-alist
-  '((python-mode . ((function_definition . tree-sitter-fold-range-python)
-                    (class_definition . tree-sitter-fold-range-python)))
-    (ess-r-mode . ((brace_list . tree-sitter-fold-range-r)))
-    (nix-mode . ((attrset . tree-sitter-fold-range-nix-attrset)
-                 (function . tree-sitter-fold-range-nix-function)))
-    (go-mode . ((type_declaration . tree-sitter-fold-range-go-type-declaration)
-                (function_declaration . tree-sitter-fold-range-go-method)
-                (method_declaration . tree-sitter-fold-range-go-method))))
+  '((python-ts-mode . (("function_definition" . tree-sitter-fold-range-python)
+                       ("class_definition" . tree-sitter-fold-range-python)))
+    (ess-r-mode . (("brace_list" . tree-sitter-fold-range-r)))
+    (nix-mode . (("attrset" . tree-sitter-fold-range-nix-attrset)
+                 ("function" . tree-sitter-fold-range-nix-function)))
+    (go-mode . (("type_declaration" . tree-sitter-fold-range-go-type-declaration)
+                ("function_declaration" . tree-sitter-fold-range-go-method)
+                ("method_declaration" . tree-sitter-fold-range-go-method))))
   "An alist of (major-mode . (foldable-node-type . function)).
 FUNCTION is used to determine where the beginning and end for FOLDABLE-NODE-TYPE
 in MAJOR-MODE.  It should take a single argument (the syntax node with type
@@ -99,20 +98,14 @@ Raise `user-error' if no foldable node is found.
 This function is borrowed from `tree-sitter-node-at-point'."
   (let* ((pos (or pos (point)))
          (foldable-types (alist-get major-mode tree-sitter-fold-foldable-node-alist))
-         (root (tsc-root-node tree-sitter-tree))
-         (node (tsc-get-descendant-for-position-range root pos pos)))
-    (let ((current node) result)
-      (while current
-        (if (memq (tsc-node-type current) foldable-types)
-            (setq result current
-                  current nil)
-          (setq current (tsc-get-parent current))))
-      (or result (user-error "No foldable node found at POS")))))
+         (node (treesit-node-at (point)))
+         (foldable-node (treesit-parent-until node (lambda (node) (member (treesit-node-type node) foldable-types)))))
+    (or foldable-node (user-error "No foldable node found at POS"))))
 
 (defun tree-sitter-fold--get-fold-range (node)
   "Return the beginning (as buffer position) of fold for NODE."
   (-if-let* ((fold-alist (alist-get major-mode tree-sitter-fold-range-alist))
-             (fn (alist-get (tsc-node-type node) fold-alist)))
+             (fn (alist-get (treesit-node-type node) fold-alist)))
       (if (functionp fn)
           (funcall fn node)
         (user-error
@@ -137,23 +130,23 @@ This function is borrowed from `tree-sitter-node-at-point'."
 (defun tree-sitter-fold-overlay-at (node)
   "Return the tree-sitter-fold overlay at NODE if NODE is foldable and folded.  Return nil otherwise."
   (-when-let* ((foldable-types (alist-get major-mode tree-sitter-fold-foldable-node-alist))
-               (_ (memq (tsc-node-type node) foldable-types))
+               (_ (memq (treesit-node-type node) foldable-types))
                (range (tree-sitter-fold--get-fold-range node)))
     (thread-last (overlays-in (car range) (cdr range))
-      (seq-filter (lambda (ov)
-                    (and (eq (overlay-get ov 'invisible) 'tree-sitter-fold)
-                         (= (overlay-start ov) (car range))
-                         (= (overlay-end ov) (cdr range)))))
-      car)))
+                 (seq-filter (lambda (ov)
+                               (and (eq (overlay-get ov 'invisible) 'tree-sitter-fold)
+                                    (= (overlay-start ov) (car range))
+                                    (= (overlay-end ov) (cdr range)))))
+                 car)))
 
 ;; ========
 ;; commands
 ;; ========
 
 (defmacro tree-sitter-fold--ensure-ts (&rest body)
-  "Run BODY only if `tree-sitter-mode' is enabled."
+  "Run BODY only if `treesit-parser-list' returns non-nil."
   (declare (indent 0))
-  `(if (bound-and-true-p tree-sitter-mode)
+  `(if (and (fboundp 'treesit-parser-list) (treesit-parser-list))
        (progn ,@body)
      (user-error "Ignored, tree-sitter-mode is not enable in the current buffer")))
 
@@ -183,25 +176,23 @@ If the current node is not folded or not foldable, do nothing."
   (interactive)
   (tree-sitter-fold--ensure-ts
     (when-let* ((node (tree-sitter-fold--foldable-node-at-pos))
-                (beg (tsc-node-start-position node))
-                (end (tsc-node-end-position node)))
+                (beg (treesit-node-start node))
+                (end (treesit-node-end node)))
       (thread-last (overlays-in beg end)
-        (seq-filter (lambda (ov) (eq (overlay-get ov 'invisible) 'tree-sitter-fold)))
-        (mapc #'delete-overlay)))))
+                   (seq-filter (lambda (ov) (eq (overlay-get ov 'invisible) 'tree-sitter-fold)))
+                   (mapc #'delete-overlay)))))
 
 (defun tree-sitter-fold-close-all ()
   "Fold all foldable syntax nodes in the buffer."
   (interactive)
   (tree-sitter-fold--ensure-ts
-    (let* ((node (tsc-root-node tree-sitter-tree))
-           (patterns (seq-mapcat (lambda (type) `(,(list type) @name))
+    (let* ((patterns (seq-mapcat (lambda (type) `(,(list type) @name))
                                  (alist-get major-mode tree-sitter-fold-foldable-node-alist)
                                  'vector))
-           (query (tsc-make-query tree-sitter-language patterns))
-           (nodes-to-fold (tsc-query-captures query node #'ignore)))
+           (nodes-to-fold (treesit-query-capture node query)))
       (thread-last nodes-to-fold
-        (mapcar #'cdr)
-        (mapc #'tree-sitter-fold-close)))))
+                   (mapcar #'cdr)
+                   (mapc #'tree-sitter-fold-close)))))
 
 (defun tree-sitter-fold-open-all ()
   "Unfold all syntax nodes in the buffer."
@@ -227,61 +218,61 @@ If the current syntax node is not foldable, do nothing."
 
 (defun tree-sitter-fold-range-python (node)
   "Return the fold range for `function_definition' and `class_definition' NODE in Python."
-  (let* ((named-node (or (tsc-get-child-by-field node :superclasses)
-                         (tsc-get-child-by-field node :return_type)
-                         (tsc-get-child-by-field node :parameters)
-                         (tsc-get-child-by-field node :name)))
+  (let* ((named-node (or (treesit-node-child-by-field-name node "superclasses")
+                         (treesit-node-child-by-field-name node "return_type")
+                         (treesit-node-child-by-field-name node "parameters")
+                         (treesit-node-child-by-field-name node "name")))
          ;; the colon is an anonymous node after return_type or parameters node
-         (beg (tsc-node-end-position (tsc-get-next-sibling named-node)))
-         (end (tsc-node-end-position node)))
+         (beg (treesit-node-start (treesit-node-next-sibling named-node)))
+         (end (treesit-node-end node)))
     (cons beg end)))
 
 (defun tree-sitter-fold-range-r (node)
   "Return the fold range for `brace_list' NODE in R."
-  (let ((beg (tsc-node-end-position (tsc-get-nth-child node 0)))
-        (end (1- (tsc-node-end-position node))))
+  (let ((beg (treesit-node-end (treesit-node-child node 0)))
+        (end (1- (treesit-node-end node))))
     (cons beg end)))
 
 (defun tree-sitter-fold-range-nix-attrset (node)
   "Return the fold range for `attrset' NODE in Nix express language."
-  (let ((beg (tsc-node-end-position (tsc-get-nth-child node 0)))
-        (end (1- (tsc-node-end-position node))))
+  (let ((beg (treesit-node-end (treesit-node-child node 0)))
+        (end (1- (treesit-node-end node))))
     (cons beg end)))
 
 (defun tree-sitter-fold-range-nix-function (node)
   "Return the fold range for `function' NODE in Nix express language."
   (let ((beg (thread-first node
-               (tsc-get-child-by-field :formals)
-               (tsc-get-next-sibling)
-               (tsc-node-end-position)))
-        (end (tsc-node-end-position node)))
+                           (treesit-node-child-by-field-name "formals")
+                           (treesit-node-next-sibling)
+                           (treesit-node-end)))
+        (end (treesit-node-end node)))
     (cons beg end)))
 
 (defun tree-sitter-fold-range-go-type-declaration (node)
   "Return the fold range for `type_declaration' NODE in Go language.
 Only `struct_type' and `interface_type' nodes can be folded."
-  (when-let* ((type-spec-node (tsc-get-nth-child node 1))
+  (when-let* ((type-spec-node (treesit-node-child node 1))
               ;; the type_spec node is not named in the Go grammar
               ;; so ensure that the 1-th child is a type_spec node
-              (_ (eq (tsc-node-type type-spec-node) 'type_spec))
-              (type-node (tsc-get-child-by-field type-spec-node :type))
-              (type-node-type (tsc-node-type type-node)))
+              (_ (eq (treesit-node-type type-spec-node) "type_spec"))
+              (type-node (treesit-node-child-by-field-name type-spec-node "type"))
+              (type-node-type (treesit-node-type type-node)))
     (cond
      ;; only struct and interface types can be folded
-     ((or (eq type-node-type 'struct_type)
-          (eq type-node-type 'interface_type))
+     ((or (eq type-node-type "struct_type")
+          (eq type-node-type "interface_type"))
       ;; find the end of the "struct" or "interface" keyword
-      (let ((beg (1+ (tsc-node-end-position (tsc-get-nth-child type-node 0))))
-            (end (tsc-node-end-position node)))
+      (let ((beg (1+ (treesit-node-end (treesit-node-child type-node 0))))
+            (end (treesit-node-end node)))
         (cons beg end)))
      (t nil))))
 
 (defun tree-sitter-fold-range-go-method (node)
   "Return the fold range for `method_declaration' NODE in Go language."
-  (let* ((named-node (or (tsc-get-child-by-field node :result)
-                        (tsc-get-child-by-field node :parameters)))
-         (beg (1+ (tsc-node-end-position named-node)))
-        (end (tsc-node-end-position node)))
+  (let* ((named-node (or (treesit-node-child-by-field-name node "result")
+                         (treesit-node-child-by-field-name node "parameters")))
+         (beg (1+ (treesit-node-end named-node)))
+         (end (treesit-node-end node)))
     (cons beg end)))
 
 (provide 'tree-sitter-fold)
